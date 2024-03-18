966a9dc5bc58ae2aa85af64e69c6b7e2baf67846
unknown <sms10086@hotmail.com>
2024-03-18 08:55:55 +0800

    commit

---
+++ others.go
@@ -0,0 +1,72 @@
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+// sms10086@hotmail.com
+
+package ffmpeg
+
+// libavcodec/d3d11va.h
+type HANDLE uintptr
+type ID3D11VideoDecoder struct {
+}
+type ID3D11VideoContext struct {
+}
+type D3D11_VIDEO_DECODER_CONFIG struct {
+}
+type ID3D11VideoDecoderOutputView struct {
+}
+
+// libcodec/dxva2.h
+type IDirectXVideoDecoder struct {
+}
+type DXVA2_ConfigPictureDecode struct {
+}
+type LPDIRECT3DSURFACE9 struct {
+}
+
+// libavfilter/avfilter.h
+type AVFilterCommand struct {
+}
+type AVFilterChannelLayouts struct {
+}
+
+// libavformat/avformat.h
+type AVIndexEntry struct {
+	Pos int64
+	Timestamp int64
+	Flags_and_size int32
+	Min_distance int32
+}
+
+type URLContext struct {
+}
+
+// libavutil/hwcontext_d3d1va.h
+type ID3D11Device struct {
+}
+type ID3D11DeviceContext struct {
+}
+type ID3D11VideoDevice struct {
+}
+type ID3D11Texture2D struct {
+}
+
+// libavcodec/videotoolbox.h
+type VTDecompressionSessionRef uintptr
+type OSType int32
+type CMVideoFormatDescriptionRef uintptr
+
+// libavcodec/xvmc.h
+type XvMCMacroBlock struct {
+}
+type XvMCSurface struct {
+}
+
+// libutil/hwcontext_dxva2.h
+type IDirect3DDeviceManager9 struct {
+}
+type IDirect3DSurface9 struct {
+}
+
+// libavcodec/avcodec.h
+type AVCodecHWConfigInternal struct {
+
+}
